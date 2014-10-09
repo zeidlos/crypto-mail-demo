5d7d03fcd4d8f4bdd706a33eef313d7db74b5089
Julius Zeidler <julius@everbase.net>
2014-10-09 15:03:43 +0200

    Added output on console

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 var io = require('socket.io')(4223);
 io.on('connection', function(socket) {
   socket.on('message', function(data) {
+    console.log(data);
     socket.broadcast.emit('message', data);
   });
 });
